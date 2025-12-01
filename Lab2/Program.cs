be00586a444162d626501e66a5a14cfed8d1f09f
diwwka <zaponka103@gmail.com>
2025-12-01 13:31:55 +0200

    Configured DbContext and connection string

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -1,6 +1,16 @@
+﻿using Lab2.dal;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// 1. Отримуємо рядок підключення з appsettings.json
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// 2. Реєструємо LabDbContext із SQL Server
+builder.Services.AddDbContext<LabDbContext>(options =>
+    options.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
